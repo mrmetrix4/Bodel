ba9dd6e6305568be1341932a09986efb4bed78a6
Adar Arbiv <mrmetrix4@gmail.com>
2023-03-02 15:31:32 +0200

    Changed prints to logs

--- bodel.py
+++ bodel.py
@@ -1,5 +1,6 @@
 import configparser
 import socket
+import logging
 from json import loads, dumps, JSONDecodeError
 
 CONFIG_FILE_PATH = "conf.ini"
@@ -9,21 +10,29 @@ GLOBAL_SETTINGS_SECTION = "GlobalSettings"
 OVERFLOW_SOCKET_ERRORNO = 10040
 
 
+logging.basicConfig(
+    filename="debug.log",
+    filemode="a",
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.DEBUG,
+)
+
+
 def safe_recv_json(receiver_socket, packet_size):
     try:
         data, client = receiver_socket.recvfrom(packet_size)
     except OSError as e:
         if e.winerror != OVERFLOW_SOCKET_ERRORNO:
             raise e
-        print(f"[*] Overflow message sent to the black core.")
-        print(f"[*] The message size was >{packet_size}.")
+        logging.debug(f"[*] Overflow message sent to the black core.")
+        logging.debug(f"[*] The message size was >{packet_size}.")
         return None
-    print(f"[*] Black core received message:")
-    print(data.decode())
+    logging.debug(f"[*] Black core received from {client} a message:")
+    logging.debug(data.decode())
     try:
         recv_json = loads(data.decode())
     except JSONDecodeError:
-        print(f"[*] JSON schema mismatch from {client}.")
+        logging.debug(f"[*] JSON schema mismatch.")
         return None
 
     return recv_json
@@ -31,8 +40,10 @@ def safe_recv_json(receiver_socket, packet_size):
 
 def send_json(sender_socket, dest_ip, dest_port, data):
     data_size = sender_socket.sendto(data.encode(), (dest_ip, dest_port))
-    print(f"[*] Red core sent {data_size} bytes sized message:")
-    print(data)
+    logging.debug(
+        f"[*] Red core sent {data_size} bytes sized message to {(dest_ip, dest_port)}:"
+    )
+    logging.debug(data)
 
 
 def exec_from_dict(d):
@@ -41,10 +52,10 @@ def exec_from_dict(d):
     try:
         exec(d["onError"])
     except Exception as e:
-        print(f"[*] Could not execute \"{d['onError']}\"")
-        print(f"[*] Exception {e}")
+        logging.debug(f"[*] Could not execute \"{d['onError']}\"")
+        logging.debug(f"[*] Exception {e}")
     else:
-        print(f"[*] Executed \"{d['onError']}\"")
+        logging.debug(f"[*] Executed \"{d['onError']}\"")
 
 
 def filter_json(sus_json):
@@ -66,7 +77,7 @@ def main():
 
     sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    print(f"[*] Opened the black core receiver on {receiver_port}/UDP.")
+    logging.debug(f"[*] Opened the black core receiver on {receiver_port}/UDP.")
 
     while True:
         if not (recv_json := safe_recv_json(receiver_socket, packet_size)):
